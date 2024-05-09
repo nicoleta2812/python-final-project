19e81999765bcd2c0a19710a3ae8bdadaaa36206
NicoletaBarbacaru <nicoletaburlacu28@yahoo.com>
2024-05-09 19:43:28 +0300

    Create buttons and labels for temperature, presure, humidity, wind, cloudness and description

--- weather.py
+++ weather.py
@@ -16,10 +16,15 @@ timelabel = Label(root, text="", bg="royal blue1", font=("bold", 14), fg="yellow
 
 button_submit = Button(root, text="Get Weather", width=10, font=12, bg="lime green", command= None)
 button_forecast = Button(root, text = "Weather forecast", width=14, font=12, bg="lime green", command= None)
-button_reset = Button(root, text = "REset", font=12, bg="lime green", command= None )
+button_reset = Button(root, text = "Reset", font=12, bg="lime green", command= None )
 
 # button_submit.pack(padx=50, pady=50)
 label2 = Label(root, text = "Temperature", font =("bold", 12), bg="red")
+label3 = Label(root, text = "Pressure", font =("bold", 12), bg="royal blue1")
+label2 = Label(root, text = "Humidity", font =("bold", 12), bg="royal blue1")
+label2 = Label(root, text = "Wind", font =("bold", 12), bg="red")
+label2 = Label(root, text = "Cloudness", font =("bold", 12), bg="red")
+label2 = Label(root, text = "Description", font =("bold", 12), bg="red")
 
 
 root.mainloop()
