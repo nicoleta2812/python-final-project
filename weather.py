3602252340dd1c75b96a759efaf5070735b365f7
NicoletaBarbacaru <nicoletaburlacu28@yahoo.com>
2024-05-09 19:34:12 +0300

    Create label for title, city name and time

--- weather.py
+++ weather.py
@@ -6,5 +6,20 @@ from datetime import datetime
 
 root = Tk()
 root.title("Weather App")
-root.configure(bg="roysl blue1")
+root.configure(bg="royal blue1")
 root.geometry("700x450")
+
+title = Label(root, text="Weather and forecast", fg="yellow", bg="royal blue1", font=("bold",15) )
+label1 = Label(root, text = "Enter the city name: ", font = ("bold", 12), bg="royal blue1")
+city_input = Entry(root, width=24, fg="red2", font=12, relief="groove")
+timelabel = Label(root, text="", bg="royal blue1", font=("bold", 14), fg="yellow")
+
+button_submit = Button(root, text="Get Weather", width=10, font=12, bg="lime green", command= None)
+button_forecast = Button(root, text = "Weather forecast", width=14, font=12, bg="lime green", command= None)
+button_reset = Button(root, text = "REset", font=12, bg="lime green", command= None )
+
+# button_submit.pack(padx=50, pady=50)
+label2 = Label(root, text = "Temperature", font =("bold", 12), bg="red")
+
+
+root.mainloop()
