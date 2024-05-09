55deb4aaae23267c2e7e6eb7c2e5754d91977899
NicoletaBarbacaru <nicoletaburlacu28@yahoo.com>
2024-05-09 18:03:56 +0300

    import tkinter, requests and pillow library and create the root window with tk function

--- weather.py
+++ weather.py
@@ -3,3 +3,8 @@ from tkinter import messagebox as mb
 import requests
 from PIL import Image
 from datetime import datetime
+
+root = Tk()
+root.title("Weather App")
+root.configure(bg="roysl blue1")
+root.geometry("700x450")
